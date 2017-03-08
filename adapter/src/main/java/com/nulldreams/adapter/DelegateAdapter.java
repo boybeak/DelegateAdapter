5818777723610666a497095088933048798823b6
boybeak <boybeak@qq.com>
2017-03-08 23:55:10 +0800

    commit nothing

--- adapter/src/main/java/com/nulldreams/adapter/DelegateAdapter.java
+++ adapter/src/main/java/com/nulldreams/adapter/DelegateAdapter.java
@@ -17,10 +17,8 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by gaoyunfei on 16/7/29.
