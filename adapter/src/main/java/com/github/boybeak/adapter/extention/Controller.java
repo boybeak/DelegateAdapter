5d546b848efeed13c9d38e8d8525cf3672b76580
boybeak <boybeak@qq.com>
2017-08-27 18:36:46 +0800

    make version 2.3.0, divide Selector into a dependent library

---
+++ adapter/src/main/java/com/github/boybeak/adapter/extention/Controller.java
@@ -0,0 +1,8 @@
+package com.github.boybeak.adapter.extention;
+
+/**
+ * Created by gaoyunfei on 2017/8/26.
+ */
+
+public interface Controller {
+}
--- selector/src/main/java/com/github/boybeak/selector/Action.java
+++
@@ -1,9 +0,0 @@
-package com.github.boybeak.selector;
-
-/**
- * Created by gaoyunfei on 2017/6/14.
- */
-
-public interface Action<T> {
-    void action (int index, T t);
-}
