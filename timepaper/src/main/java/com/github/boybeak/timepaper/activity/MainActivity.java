f1a338ce237e1c8c43752d371975fcf9d7540b06
boybeak <boybeak@qq.com>
2017-09-15 21:02:02 +0800

    update

--- timepaper/src/main/java/com/github/boybeak/timepaper/activity/MainActivity.java
+++ timepaper/src/main/java/com/github/boybeak/timepaper/activity/MainActivity.java
@@ -120,8 +120,8 @@ public class MainActivity extends BaseActivity {
         mTl.setupWithViewPager(mVp);
 
         getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE |
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         );
 
         mTl.addOnTabSelectedListener(mTabListener);
